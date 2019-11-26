6e4fc0b699bc09abae6dff7cad77e320ecf41d4c
Dragan Radisic <dragan@presta-ecommerce.com>
2019-11-26 20:54:26 +0100

    redirect to root page on auth Controllers

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/ad';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
